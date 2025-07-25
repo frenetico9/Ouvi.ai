e3a3ccddeaff3a844669eea5c60a47f9fdfc8bc5
frenetico9 <hiago182016@gmail.com>
2025-07-25 15:32:37 -0300

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -67,7 +67,7 @@ const API_KEYS = [
   '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',
   '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',
 ];
-const MODEL_NAME = 'meta-llama/llama-3.2-11b-vision-instruct:free';
+const MODEL_NAME = 'deepseek/deepseek-r1-distill-llama-70b:free';
 const OPENROUTER_URL = 'https://openrouter.ai/api/v1/chat/completions';
 let requestCounter = 0;
 
